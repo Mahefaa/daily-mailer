50ac9ed8e5da8d5e31e9c30d091c7a2a682d3b1e
Mahefaa <hei.mahefa@gmail.com>
2025-06-03 10:36:45 +0300

    wip: rm unused files

--- src/main/java/com/mailer/daily/handler/model/ErrorModel.java
+++
@@ -1,7 +0,0 @@
-package com.mailer.daily.handler.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.mailer.daily.PojaGenerated;
-
-@PojaGenerated
-public record ErrorModel(@JsonProperty("message") String message) {}
